44c7533fd2b4b7c1bc9912e3f03c0dc629c68556
user <nicolas9101@yahoo.com>
2015-12-04 11:01:08 -0700

    added test case for area of triangle

--- geometry.c
+++ geometry.c
@@ -69,3 +69,13 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     mid->y = ((a->y + b->y) / 2.0 );
 
 }
+double coord_2d_area_triangle(const	coord_2d_t*	a, const coord_2d_t* b, const coord_2d_t* c){
+	double p1 = a->x * (b->y - c->y);
+	double p2 = b->x * (c->y - a->y);
+	double p3 = c->x * (a->y - b->y);
+	
+	double sum = (p1 + p2 + p3)/2;
+	double answer = fabs(sum);
+	
+	return answer;
+}
